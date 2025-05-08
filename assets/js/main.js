5223e9f18dc616ff426ef8e0ad87d30bbfd69f82
Jeongsua717 <sua0000717@gmail.com>
2025-05-08 16:21:48 +0900

    수정
    수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
-        const menu = document.querySelector('.navbar__menu');
+const menu = document.querySelector('.navbar__menu');
 
-        toggleBtn.addEventListener('click',()=>{
-            menu.classList.toggle('active');
-        })
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
