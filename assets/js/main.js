a84116b1e2d4db2add8776463ed15b2c55b0bb4d
Jeongsua717 <sua0000717@gmail.com>
2025-05-12 12:18:12 +0900

    수정
    수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,30 @@
-const toggleBtn = document.querySelector('.navbar-toggler');
-const menu = document.querySelector('.navbar__menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-})
+const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.getElementById('navbarNavUser');
+    navbarToggler.addEventListener('click', function() {
+    navbarCollapse.classList.toggle('show');
+    });
+    const navLinks = document.querySelectorAll('#navbarNavUser .nav-link');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            navbarCollapse.classList.remove('show');
+        });
+    });
 let swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,
     centeredSlides: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
+    }
+});
+window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('.navbar');
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 100) {
+        navbar.classList.add('scrolled-navbar');
+        navbar.classList.remove('transparent-navbar');
+    } else {
+        navbar.classList.remove('scrolled-navbar');
+        navbar.classList.add('transparent-navbar');
     }
 });
