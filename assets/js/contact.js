397658dc51446139ebd27e93ea1ee7c1f923db82
Jeongsua717 <sua0000717@gmail.com>
2025-06-01 22:54:29 +0900

    수정
    수정

---
+++ assets/js/contact.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('.navbar');
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 100) {
+        navbar.classList.add('scrolled-navbar');
+        navbar.classList.remove('transparent-navbar');
+    } else {
+        navbar.classList.remove('scrolled-navbar');
+        navbar.classList.add('transparent-navbar');
+    }
+});
